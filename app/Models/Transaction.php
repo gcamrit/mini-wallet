5ebf19c334ed962cc8bf3823bcf5c54301e936e6
Amrit G.C. <amriitgc@gmail.com>
2025-10-09 02:39:12 +0545

    fix(transaction): creates transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [];
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
